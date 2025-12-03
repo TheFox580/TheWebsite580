4c79ab639c8c95574d71c0c6434a1e67da918cf3
Fox <44817153+TheFox580@users.noreply.github.com>
2025-12-03 02:34:28 +0100

    this one is the one

--- mcsr/widget/widget.js
+++ mcsr/widget/widget.js
@@ -130,16 +130,7 @@ function updateRank(newRank) {
 }
 
 async function updateScores() {
-  const response = await fetch("https://api.mcsrranked.com/users/TheFox580", {
-    method: "GET",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
-    headers: new Headers({
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    }),
-  });
+  const response = await fetch("https://api.mcsrranked.com/users/TheFox580");
   if (response.ok) {
     let json = await response.json();
     json = json.data;
