536e68450a4a71e41a5f5af3a4ff8ea9cbff4e16
Fox <zoelliotmitong@gmail.com>
2026-04-25 03:27:08 +0200

    Actually none were working

--- src/routes/mcsr/widget/[user]/+page.server.ts
+++ src/routes/mcsr/widget/[user]/+page.server.ts
@@ -1,6 +1,7 @@
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
+  const user = params.user;
   const response = await fetch(`https://api.mcsrranked.com/users/${user}`);
 
   const data = await response.json();
