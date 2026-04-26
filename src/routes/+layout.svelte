d875c96c77622affb1f0739052fd332c13919fd6
Fox <zoelliotmitong@gmail.com>
2026-04-26 05:14:16 +0200

    trying fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
     import "./layout.css";
     import favicon from "$lib/assets/favicon.ico";
-    import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-    import { inject } from "@vercel/analytics";
-
-    inject();
-    injectSpeedInsights();
 
     let { children } = $props();
 </script>
