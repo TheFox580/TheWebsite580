e097f767f34c23da86ad4c7ba96bb0ce879039ac
Fox <zoelliotmitong@gmail.com>
2026-04-26 05:06:21 +0200

    Added speed insights & analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-	import "./layout.css";
-	import favicon from "$lib/assets/favicon.ico";
+    import "./layout.css";
+    import favicon from "$lib/assets/favicon.ico";
+    import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+    import { inject } from "@vercel/analytics";
 
-	let { children } = $props();
+    inject();
+    injectSpeedInsights();
+
+    let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
