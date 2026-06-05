15e1253822942dbeb1c3b104bc90d7e8624ef1c4
Fox <zoelliotmitong@gmail.com>
2026-06-05 16:49:53 +0200

    changed redirection to the donation page

--- src/routes/donate/+page.server.ts
+++ src/routes/donate/+page.server.ts
@@ -2,5 +2,8 @@ import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-  redirect(303, "https://tiltify.com/@thefox580/streamers-against-cancer-4");
+  redirect(
+    303,
+    "https://donate.tiltify.com/6310da27-c997-43d7-8671-c4ace2722009/incentives",
+  );
 };
