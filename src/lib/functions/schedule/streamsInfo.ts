11e27c1e99b3b95eef84f2d8ce61f021511897aa
Fox <zoelliotmitong@gmail.com>
2026-07-19 03:16:00 +0200

    Week starts on Sunday OF COURSE WHY WOULD IT START ON MONDAY HAHAHAHAHAHAHAHAAHHAAHHAHA

--- src/lib/functions/schedule/streamsInfo.ts
+++ src/lib/functions/schedule/streamsInfo.ts
@@ -1,16 +1,16 @@
 import type { Stream } from "$lib/interfaces/schedule/Schedule";
 
 export function getDay(week_day: number) {
-  while (week_day < 1) week_day += 7;
-  while (week_day > 7) week_day -= 7;
-  switch (week_day){
+  while (week_day < 0) week_day += 7;
+  while (week_day > 6) week_day -= 7;
+  switch (week_day) {
+    case 0: return "Sunday"
     case 1: return "Monday";
     case 2: return "Tuesday";
     case 3: return "Wednesday";
     case 4: return "Thrusday";
     case 5: return "Friday";
     case 6: return "Satruday";
-    case 7: return "Sunday";
     default: return "None"
   }
 }
