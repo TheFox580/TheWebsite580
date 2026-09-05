02a53e77837a441af3018ac4de4ea8525408be95
Fox <zoelliotmitong@gmail.com>
2026-09-05 05:52:54 +0200

    only redirects to my stats Until I find a way to not have bots / AI use my token to scrap the MCCI API because they don't know how to code L bozo

--- src/routes/mcci/+page.ts
+++ src/routes/mcci/+page.ts
@@ -2,5 +2,5 @@ import { redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = () => {
-  redirect(303, "/");
+  redirect(307, "/mcci/TheFox580");
 };
