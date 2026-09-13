79a605675f13439394188e21e460da0c1b8f3139
Fox <zoelliotmitong@gmail.com>
2026-09-13 04:08:43 +0200

    Fixed graph looking weird on mobile

--- src/routes/chunk/+page.svelte
+++ src/routes/chunk/+page.svelte
@@ -3,6 +3,7 @@
     import {Chart as ChartJS, Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale, scales} from "chart.js/auto"
     import type { PageData } from "./$types";
     import { dev } from "$app/environment";
+    import { z } from "$lib/functions/utils/numberFormatting";
     import type { MinedData } from "$lib/interfaces/chunk/MinedData";
     import { onMount } from "svelte";
     import { getMonth } from "$lib/functions/utils/dateStuff";
@@ -22,6 +23,8 @@
         CategoryScale
     );
 
+    let innerWidth = $state()
+    let innerHeight = $state()
     let mined_data: MinedData[] = $state(data.mined_data);
     let graph_data = $state();
     let last_data: number = $state(mined_data.findLast(mined => mined.max > 0));
@@ -46,7 +49,7 @@
         max_blocks = last_data ? last_data.max : 0;
 
         graph_data = {
-            labels: mined_data.map(mined => `${getMonth(mined.time.getMonth())} ${mined.time.getDate()} at ${mined.time.getHours()}:${mined.time.getMinutes()}`),
+            labels: mined_data.map(mined => `${getMonth(mined.time.getMonth())} ${mined.time.getDate()} at ${z(mined.time.getHours())}:${z(mined.time.getMinutes())}`),
             datasets: [
                 {
                     label: "Blocks Mined",
@@ -64,19 +67,21 @@
             const time = new Date();
             if (time.getMinutes() % 5 == 0 && time.getSeconds() == 0) await fetchMinedData();
         },
-            5*60*1000) // Every 5 minutes
+            1000) // Every second
     })
 
     formatData();
 
 </script>
 
-<div class="w-full h-screen flex flex-col items-center justify-center bg-black">
+<svelte:window bind:innerWidth bind:innerHeight />
+
+<div class="w-full h-screen flex flex-col items-center justify-center bg-black text-center">
     <div class="h-1/5 flex flex-col items-center justify-center text-white text-5xl">
         <h1 class="mb-5">Fox's chunk mining progress</h1>
         <h2 class="text-3xl">{formatNumber(last_data ? last_data.blocks_mined : 0)} / {formatNumber(max_blocks)} blocks mined</h2>
     </div>
-    <div class="h-4/5 w-300 h-160 flex flex-col items-center justify-center">
+    <div class="h-4/5 h-160 flex flex-col items-center justify-center" style="width: {innerWidth}px;">
         {#if last_data}
         <div class= "w-full h-10 flex flex-row items-center justify-center text-white text-3xl">
             <h2>Blocks mined over time</h2>
