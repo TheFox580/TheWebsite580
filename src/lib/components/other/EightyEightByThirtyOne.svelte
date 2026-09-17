6f1e1d99e9aac4c326bf26e3c986b75ce3446ddf
Fox <zoelliotmitong@gmail.com>
2026-09-17 23:35:29 +0200

    added a new little guy

--- src/lib/components/other/EightyEightByThirtyOne.svelte
+++ src/lib/components/other/EightyEightByThirtyOne.svelte
@@ -8,8 +8,9 @@
     {img: "twitch.png", url: "https://twitch.tv/thefox580"},
     {img: "github.png", url: "https://github.com/TheFox580"},
     {img: "modrinth.webp", url: "https://modrinth.com/user/TheFox580"},
-    {img: "bluesky.png", url: "https://bsky.app/profile/thefox580.bsky.social"},
-    {img: "fantomitechno.webp", url: "https://simon.renoux.dev/"},
+    {img: "bluesky.png", url: "https://bsky.app/profile/thefox580.me"},
+    {img: "fantomitechno.webp", url: "https://fantomitechno.dev/"},
+    {img: "risingsunlight.png", url: "https://blog.risingsunlight.dev/"},
     {img: "eva.ac-badge.png", url: "https://eva.ac"},
     {img: "minecraft.gif", url: "https://minecraft.net"},
     {img: "Archipelago.png", url: "https://archipelago.gg"},
@@ -32,6 +33,9 @@
 </div>
 
 <style>
+  .eightyeightbythirtyone {
+      border: solid black 2px;
+  }
   .eightyeightbythirtyone:hover {
     border: solid white 2px;
   }
