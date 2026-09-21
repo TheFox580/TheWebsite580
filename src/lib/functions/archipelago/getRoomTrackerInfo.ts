8852651cd24c201aa8f5426e25b8774aac64bcf3
TheFox580 <zoelliotmitong@gmail.com>
2026-09-21 10:28:30 +0200

    Use dev url when in dev mode

--- src/lib/functions/archipelago/getRoomTrackerInfo.ts
+++ src/lib/functions/archipelago/getRoomTrackerInfo.ts
@@ -1,10 +1,12 @@
 import type { PlayerSlotInfo } from "$lib/interfaces/archipelago/PlayerSlotInfo";
+import { dev } from "$app/environment";
 
 export async function getRoomTrackerInfo(
   id: string,
 ): Promise<PlayerSlotInfo[]> {
+
   const res = await fetch(
-    `https://thefox580-backend.zoelliotmitong.workers.dev/api/archipelago/tracker/${id}`,
+    dev ? `http://127.0.0.1:8787/api/archipelago/tracker/${id}` : `https://thefox580-backend.zoelliotmitong.workers.dev/api/archipelago/tracker/${id}`,
   );
 
   const players = (await res.json()).players;
